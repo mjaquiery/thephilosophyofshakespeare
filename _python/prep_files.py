5cc97715e2107055870356f39ea38756e162c937
Matt Jaquiery <matt.jaquiery@cs.ox.ac.uk>
2023-07-22 19:52:14 +0100

    content

--- _python/prep_files.py
+++ _python/prep_files.py
@@ -6,11 +6,11 @@ import docx
 
 # Walk through .docx files in ./src/_raw and remove those with corresponding
 # .md files in ./src/posts
-for root, dirs, files in os.walk('./src/_raw/publish'):
+for root, dirs, files in os.walk('./src/_raw'):
     for file in files:
         if file.endswith('.docx'):
             docx_file = os.path.join(root, file)
-            md_file = os.path.join('./src/posts/publish', f"{file}.md")
+            md_file = os.path.join('./src/posts', f"{file}.md")
             if os.path.isfile(md_file):
                 os.remove(docx_file)
                 print('Removed: ' + docx_file)
